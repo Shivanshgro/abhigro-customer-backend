99104fb42f28e68adff9659e82f026939a65f5b3
ABHISEHKBACHANNA <Abhishek.bachanna@revappayyaitservices.com>
2026-06-28 21:45:20 +0530

    revert: rollback to working state before catalog changes

--- src/routes/catalogRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const cat = require("../controllers/catalog/structure")
-
-router.get("/categories", cat.getCategories)
-router.get("/subcategories", cat.getSubcategories)
-router.get("/brands", cat.getBrands)
-
-module.exports = router
