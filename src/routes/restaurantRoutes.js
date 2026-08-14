6cde79c67af2b6b0030e9022c5622df10384cbf5
ABHISEHKBACHANNA <Abhishek.bachanna@revappayyaitservices.com>
2026-08-14 08:36:58 +0530

    Add offers, reviews and reports endpoints for the partner panel

--- src/routes/restaurantRoutes.js
+++ src/routes/restaurantRoutes.js
@@ -5,6 +5,7 @@ const registerRestaurant = require("../controllers/restaurant/registerRestaurant
 const { myRestaurant, setOnline } = require("../controllers/restaurant/myRestaurant")
 const menu = require("../controllers/restaurant/menu")
 const ro = require("../controllers/restaurant/restaurantOrders")
+const extras = require("../controllers/restaurant/restaurantExtras")
 
 router.post("/register", registerRestaurant)
 router.get("/me", auth, myRestaurant)
@@ -26,5 +27,18 @@ router.post("/orders/:id/reject", auth, ro.reject)
 router.post("/orders/:id/preparing", auth, ro.preparing)
 router.post("/orders/:id/ready", auth, ro.ready)
 router.get("/payouts", auth, ro.payouts)
+
+// Offers
+router.get("/offers", auth, extras.getOffers)
+router.post("/offers", auth, extras.createOffer)
+router.post("/offers/:id/toggle", auth, extras.toggleOffer)
+router.delete("/offers/:id", auth, extras.deleteOffer)
+
+// Reviews
+router.get("/reviews", auth, extras.getReviews)
+router.post("/reviews/:id/reply", auth, extras.replyReview)
+
+// Reports
+router.get("/reports", auth, extras.getReports)
 
 module.exports = router
